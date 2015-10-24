8da76550bf9573774bc17d911f0097eee7f07094
PhilippObergfell <philippobergfell@web.de>
2015-10-24 16:26:33 +0200

    New1

--- Exercise1/HelloWorld/src/HelloWorld.java
+++ Exercise1/HelloWorld/src/HelloWorld.java
@@ -14,18 +14,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		
-		Scanner user_input = new  Scanner (System.in);
-		String userName;
-
 		
-		System.out.println("Hello ASE2015 - how are you now? :)");
 
 		System.out.println("Hello ASE2015 - how are you? :)");
 
-		System.out.println("Please enter your name ! :)");
 		
-		userName = user_input.next();
-		System.out.println("Hello" + userName);
 	}
 
 }
