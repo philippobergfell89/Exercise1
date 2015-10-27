071f652dd8010a57b8b447f584f343a011084ff7
PhilippObergfell <philippobergfell@web.de>
2015-10-27 20:08:11 +0100

    20:08

--- Exercise1/HelloWorld/src/HelloUser.java
+++ Exercise1/HelloWorld/src/HelloUser.java
@@ -13,9 +13,10 @@ public class HelloUser
  
 	public void greetUser()
 	{
-	 System.out.println("Please enter your name :)");
+	 
 	 Scanner scanner1 = new Scanner( System.in );
 	 this.userName = scanner1.nextLine();
 	 System.out.print("Hello " + userName);
 	}
+ 
 }
--- Exercise1/HelloWorld/src/HelloWorld.java
+++ Exercise1/HelloWorld/src/HelloWorld.java
@@ -12,6 +12,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
 		System.out.println("Hello ASE2015 - how are you? :)");
 		HelloUser newuser = new HelloUser("InitialValue");
 		newuser.greetUser();
